899a2d1a4fcd65d9d02ff08b53a015261fec448b
Rompich <146632414+Rompich@users.noreply.github.com>
2024-02-20 14:30:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import Flask, render_template, request, redirect
+from flask import json
+from urllib.request import urlopen
+import sqlite3
 
 app = Flask(__name__) #creating flask app name
 
@@ -17,6 +21,17 @@ def resume_2():
 @app.route('/resume_template')
 def resume_template():
     return render_template("resume_template.html")
-
+    
+# Création d'une nouvelle route pour la lecture de la BDD
+@app.route("/consultation/")
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
 if(__name__ == "__main__"):
     app.run()
