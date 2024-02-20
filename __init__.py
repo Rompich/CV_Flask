c1628925a2b40d3aac175640721f62c709bbf9d5
Rompich <146632414+Rompich@users.noreply.github.com>
2024-02-20 16:04:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import Flask, render_template, request, redirect
-from flask import json
-from urllib.request import urlopen
+from flask import Flask, render_template, request, redirect, jsonify, json
 import sqlite3
+from urllib.request import urlopen
 
+def get_db_connection():
+    conn = sqlite3.connect('/home/yandj/www/flask/database.db')  # Remplacez 'database.db' par le chemin de votre base de données SQLite.
+    conn.row_factory = sqlite3.Row  # Accès aux colonnes par nom.
+    return conn
+    
 app = Flask(__name__) #creating flask app name
 
 @app.route('/')
@@ -21,28 +24,18 @@ def resume_2():
 @app.route('/resume_template')
 def resume_template():
     return render_template("resume_template.html")
-    
+
 # Création d'une nouvelle route pour la lecture de la BDD
 @app.route("/consultation/")
 def ReadBDD():
-    conn = sqlite3.connect('/home/yandj/www/flask/database.db')
+    conn = get_db_connection()  # Utilisation de la fonction définie pour la connexion
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients;')
+    cursor.execute('SELECT * FROM livres;')
     data = cursor.fetchall()
     conn.close()
     
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
-    
-@app.route('/fiche_client/<int:post_id>')
-def Readfiche(post_id):
-    conn = sqlite3.connect('/home/yandj/www/flask/database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
-    data = cursor.fetchall()
-    conn.close()
-    
-    # Rendre le template HTML et transmettre les données
-    return render_template('read_data.html', data=data)
+
 if(__name__ == "__main__"):
     app.run()
